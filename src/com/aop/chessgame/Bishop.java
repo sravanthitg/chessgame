2682dec54ef6d62a5c45712881d164d44dfbeeca
sravanthitg <sravanthigmrit@gmail.com>
2016-04-05 16:23:36 +0530

    source file

--- src/com/aop/chessgame/Bishop.java
+++ src/com/aop/chessgame/Bishop.java
@@ -7,7 +7,48 @@ public class Bishop extends Coin {
 
 	@Override
 	public boolean canMove(int dest, ChessGame game) {
-		// TODO Auto-generated method stub
+		if (isDiagnalAndFree(getPosition(), dest, game)) {
+			return true;
+		}
 		return false;
+	}
+
+	private boolean isDiagnalAndFree(int position, int dest, ChessGame game) {
+		int curRow=getRow(position);
+		int curColumn=getColumn(position);
+		int destRow=getRow(dest);
+		int destColumn=getColumn(dest);
+		
+		if(game.getCoin(dest)==null && this.isWhite()!=game.getCoin(dest).isWhite())
+		{
+		if(Math.abs(curRow-destRow) == Math.abs(curColumn-destColumn))
+		{
+			for(int i=Math.min(curRow, destRow)+1; i< Math.max(curRow, destRow); i++)
+				{
+					for(int j=Math.min(curColumn, destColumn)+1;j< Math.max(curColumn, destColumn); j++)
+						{
+							if(game.getCoin(getIndex(i,j)) !=null)
+							{
+								return false;
+							}
+						}
+				}
+		 
+		}
+		else
+		{
+			return false;
+		}
+		return true;
+		}
+		else
+			return false;
+		
+	}
+
+
+	private int getIndex(int i, int j) {
+		
+		return (i*8+j);
 	}
 }
